1d3e8aebbe24cbd26838289f7a0f02fda6c850b0
Stefan Brus <stefan1brus@gmail.com>
2014-04-19 01:12:00 +0100

    Pages branch should only contain contents of dist folder
    ^^^^^

--- app.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var port = process.env.PORT || 5000;
-var app = express();
- 
-app.use(express.static(__dirname + "/dist"));
-
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
